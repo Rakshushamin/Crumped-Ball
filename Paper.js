341ee71874ee79bae3b7f08bda3540c8451b36ab
Rakshushamin <75742018+Rakshushamin@users.noreply.github.com>
2021-03-01 18:39:52 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,32 @@
+class Paper {
+    constructor(x, y, radius) {
+      var options = {
+          'isStatic':false,
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
+      }
+      this.x=x;
+      this.y=y;
+      this.image = loadImage("paper.png");
+      this.radius=radius;
+      this.body = Bodies.circle(this.x,this.y,this.radius,options);
+      
+      
+      World.add(world, this.body);
+    }
+    display()
+    {
+      var pos =this.body.position;
+      //pos.x=mouseX;
+      //pos.y=mouseY;
+      push();
+      var angle = this.body.angle;
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(RADIUS);
+      //fill("red");
+      image(this.image,0,0,this.radius,this.radius);
+      pop();
+    }
+}
